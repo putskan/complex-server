b8cfb20c21b133441bd97aa320070dbbe0ea9845
putskan <ben.lahav101@gmail.com>
2022-02-12 19:52:17 +0200

    add: whitenoise instead of AWS

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,8 +135,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
